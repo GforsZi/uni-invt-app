45841da4fbbb0edb0e3ad1e5077c548a0d5234c2
GforsZi <givaldigumelarsetiawan@gmail.com>
2025-06-03 09:41:50 +0700

    minor change

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,7 +3,7 @@
   <div class="container-fluid">
     <!--begin::Start Navbar Links-->
     <ul class="navbar-nav">
-      @if (auth()->user()?->roles['rl_name'] == 'sapras')
+      @if (auth()->user()?->roles['rl_name']??'user' == 'sapras')
       <li class="nav-item">
         <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button">
           <i class="bi bi-list"></i>
